abf86fd6dfa4ea2902c92f164dc54b0628187abc
karen S <buzz.kyt@gmail.com>
2018-07-19 21:37:50 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+var katzDeliLine= [];
+
+function takeANumber(katzDeliLine,name) {
+    katzDeliLine.push(name);
+    return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name,0)+1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0 ) { 
+  console.log(`Currently serving ${katzDeliLine[0]}.`);
+  katzDeliLine.shift();
+    } else {
+  return ("There is nobody waiting to be served!");
+}
+  return katzDeliLine;
+}
+
+function currentLine(katzDeliLine) {
+  var newDeliLine = [];
+  do{
+    for(let i = 0; i < newDeliLine.length ; i++){
+    newDeliLine = [...newDeliLine, `${i+1}. ${katzDeliLine[i]}`];
+     } return `The line is currently is: ${newDeliLine}`;
+    } while (newDeliLine.length < 1)
+      return "The line is currently empty.";
+}
+ 
